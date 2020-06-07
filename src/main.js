b5428129ac2f641948f141faad0c41b2ac7619e4
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-07 13:03:41 +0300

    Added: Connect Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuelidate from "vuelidate";
 
-Vue.config.productionTip = false
+import App from './components/App.vue'
 
+Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
