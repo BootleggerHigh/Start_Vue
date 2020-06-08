c407ea95f7485553bbd8c0c42087d78194c722d7
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-08 14:55:13 +0300

    Update: Import&&Used components

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,65 +1,29 @@
 <template>
-    <div class="container pt-2">
-        <div class="form-group">
-            <label for="name">Car Name</label>
-            <input id="name" type="text" class="form-control" v-model.trim="carName">
-
-        </div>
-
-        <div class="form-group">
-            <label for="year">Car year</label>
-            <input id="year" type="number" class="form-control" v-model.number="carYear">
-        </div>
-        <button class="btn btn-primary" @click="createCar">Create Car</button>
-        <button class="btn btn-success" @click="loadCars">Load Cars</button>
+    <div class="container text-center pt-5">
+        <h1>{{title}}</h1>
         <hr>
-        <ul class="list-group">
-            <h3> Full list Cars</h3>
-            <li class="list-group-item"
-                v-for="car of cars" :key="car.id"> <b>{{car.name}}</b> - {{car.year}}</li>
-        </ul>
+        <app-counter></app-counter>
+        <app-second-counter></app-second-counter>
+        <hr>
+        <app-actions></app-actions>
     </div>
 </template>
 
 <script>
+    import Counter from "./Counter";
+    import Actions from "./Actions";
+    import SecondCounter from "./SecondCounter";
     export default {
-        data()
-        {
-            return{
-                carName:'',
-                carYear:0,
-                cars:[],
-                resource:null,
-            }
+        components: {
+            appCounter: Counter,
+            appActions: Actions,
+            appSecondCounter:SecondCounter,
         },
-        methods:{
-           async createCar()
+        computed:{
+            title()
             {
-                const car = {
-                    name:this.carName,
-                    year:this.carYear,
-                };
-                try {
-                    await this.resource.save({},car);
-                }
-                catch (e) {
-                    console.dir(e.message);
-                }
+                return this.$store.getters.title
             },
-            async loadCars()
-            {
-                try {
-                    const get_cars_request = await this.resource.get();
-                    this.cars = await get_cars_request.json();
-                }
-                catch (e) {
-                    console.dir(e.message);
-                }
-            },
-        },
-        created()
-        {
-            this.resource = this.$resource('cars');
         }
     }
 </script>
