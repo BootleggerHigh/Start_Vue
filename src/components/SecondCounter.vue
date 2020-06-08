7ed19760b694d08a627354b9fc86eb845570883d
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-08 14:53:42 +0300

    Added SecondCounter:show second_counter

---
+++ src/components/SecondCounter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h2>Second Counter: {{counter}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SecondCounter",
+        computed:{
+            counter()
+            {
+                return this.$store.getters.computedCounter;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
