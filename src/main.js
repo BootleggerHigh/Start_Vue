cb81cb37c749d1320efe7e5e68b4e0cb47776855
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-08 08:56:55 +0300

    Added: ...other

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
-import Vuelidate from "vuelidate";
+import VueRouter from "vue-router";
+import router from "./routes/routes";
+import App from './components/App.vue';
 
-import App from './components/App.vue'
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
-Vue.use(Vuelidate);
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
 
